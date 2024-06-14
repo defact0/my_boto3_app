9bdbf070b4eece739a53806d0e561e43f500e45e
defact0 <jdlee.geek@gmail.com>
2024-06-15 00:06:04 +0900

    fix: AccessKeyMetadata related logic modification and verification

--- src/main.py
+++ src/main.py
@@ -5,7 +5,11 @@ from datetime import datetime
 def main():
     ACCESS_KEY = 'YOUR_ACCESS_KEY_ID' # YOUR_ACCESS_KEY_ID
     SECRET_KEY = 'YOUR_SECRET_ACCESS_KEY' # YOUR_SECRET_ACCESS_KEY
-    INPUT_HOURS = 5 # HOURS
+    INPUT_HOURS = 25000 # HOURS
+    
+    # Convert current date and time string to standard format
+    current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_datetime = datetime.strptime(current_timestamp, "%Y-%m-%d %H:%M:%S")
    
     # Getting access key age AWS Boto3
     #  reference - https://stackoverflow.com/questions/45156934/getting-access-key-age-aws-boto3
@@ -15,38 +19,31 @@ def main():
 
     # Get a list of IAM users
     response = iam.list_users()
-
     for user in response['Users']:
         # Get list of access keys by user
-        res = iam.list_access_keys(UserName=user['UserName'])
-
-        try:
-            # Check if CreateDate information exists
-            if res['AccessKeyMetadata'][0]['CreateDate'].date():
-                # user information
-                user_accesskeyid = res['AccessKeyMetadata'][0]['AccessKeyId']
-                user_id = user['UserId']
-
+        response_access_key = iam.list_access_keys(UserName=user['UserName'])
+        UserId = user['UserId']
+        
+        for access_key in response_access_key['AccessKeyMetadata']:
+            UserName = access_key['UserName']
+            AccessKeyId = access_key['AccessKeyId']
+            Status = access_key['Status']
+            CreateDate = access_key['CreateDate']
+            
+            if 'Active' == Status:
                 # Convert creation date string to standard format
-                accesskey_timestamp = res['AccessKeyMetadata'][0]['CreateDate']
-                accesskey_timestamp = accesskey_timestamp.strftime("%Y-%m-%d %H:%M:%S+00:00")
-                accesskey_datetime = datetime.strptime(accesskey_timestamp, "%Y-%m-%d %H:%M:%S+00:00")
-
-                # Convert current date and time string to standard format
-                current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S+00:00")
-                current_datetime = datetime.strptime(current_timestamp, "%Y-%m-%d %H:%M:%S+00:00")
+                access_key_timestamp = access_key['CreateDate']
+                access_key_timestamp = access_key_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+                access_key_datetime = datetime.strptime(access_key_timestamp, "%Y-%m-%d %H:%M:%S")
 
                 # Units of time elapsed since access key creation
-                time_delta = current_datetime - accesskey_datetime
+                time_delta = current_datetime - access_key_datetime
                 hours_difference = time_delta.total_seconds() / 3600
                 hours_difference_int = int(hours_difference)
-
+                                
                 # Return the User ID and Access Key ID of IAM Users that have been inactive for more than N hours.
                 if INPUT_HOURS < hours_difference_int:
-                    print('[DEBUG] user_id: '+ user_id + ' \t user_accesskeyid: '+ user_accesskeyid)
-        except:
-            pass
-
+                    print(UserId + ", " + AccessKeyId)
 
 if __name__ == '__main__':
     main()
